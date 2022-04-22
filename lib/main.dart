057167e4dd6f0d96a01e3062b3048e9f4832f40e
Michael R. Taylor <michael@atomic.city>
2022-04-22 00:11:07 -0500

    Filled out the main screen some.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:as_themer/automation_studio/as_config.dart';
 import 'package:as_themer/ui/version_dialog.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  windowManager.waitUntilReadyToShow().then((_) async{
+    // Hide window title bar
+    await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
+  });
+
   var configs = await AutomationStudioConfig.findAutomationStudioVersions();
   runApp(MyApp(configs: configs));
 }
@@ -11,24 +20,14 @@ class MyApp extends StatelessWidget {
   final List<AutomationStudioConfig> configs;
   const MyApp({Key? key, required this.configs}) : super(key: key);
   
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return FluentApp(
-      title: 'Flutter Demo',
+      title: 'Automation Studio Themer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         accentColor: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page', configs: configs),
+      home: MyHomePage(title: 'Automation Studio Themer', configs: configs),
     );
   }
 }
@@ -37,34 +36,96 @@ class MyHomePage extends StatefulWidget {
   final List<AutomationStudioConfig> configs;
   const MyHomePage({Key? key, required this.title, required this.configs}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WindowListener {
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Button(
-      child: const Text('Press Me'),
-      onPressed: () async {
-        var theme = await showDialog<AutomationStudioConfig>(
-            context: context,
-            builder: (context) {
-              return SelectVersionDialog(configs: widget.configs);
-            }
-        );
-      },
+    return NavigationView(
+      appBar: NavigationAppBar(
+          title: DragToMoveArea(
+              child: Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Text(widget.title, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  )
+              )
+          ),
+          automaticallyImplyLeading: false,
+          actions: DragToMoveArea(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [Spacer(), WindowButtons()],
+            ),
+          )
+      ),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          CommandBar(
+            primaryItems: [
+              CommandBarButton(
+                  icon: const Icon(FluentIcons.open_file),
+                  label: const Text('Load'),
+                  onPressed: () async {
+                    var theme = await showDialog<AutomationStudioConfig?>(
+                        context: context,
+                        builder: (context) {
+                          return SelectVersionDialog(configs: widget.configs);
+                        }
+                    );
+                    setState(() {
+
+                    });
+                  }
+              ),
+              CommandBarButton(
+                  icon: const Icon(FluentIcons.save),
+                  label: const Text('Save'),
+                  onPressed: () {
+
+                  }
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = FluentTheme.of(context);
+
+    return SizedBox(
+      width: 138,
+      height: 30,
+      child: WindowCaption(
+        brightness: theme.brightness,
+        backgroundColor: Colors.transparent,
+      ),
     );
   }
 }

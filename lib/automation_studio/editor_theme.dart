c493b1355dd8845862001c7fd0ea130c4292a82b
Michael R. Taylor <michael@atomic.city>
2022-04-26 23:59:35 -0500

    Major refactoring to EditorTheme class to better fit StateNotifierProvider usage.

--- lib/automation_studio/editor_theme.dart
+++ lib/automation_studio/editor_theme.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 import 'package:xml/xml.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/painting.dart';
 
-enum Component {
+enum EditorComponent {
   dataType,
   number,
   invalidKeyword,
@@ -24,87 +25,88 @@ enum Component {
   includeFiles,
 }
 
+@immutable
 class EditorTheme {
-  static const Color defaultForeground = Color.fromARGB(255, 0, 0, 0);
-  Color defaultBackground = const Color.fromARGB(255, 255, 255, 255);
-  Color monitorBackground = const Color.fromARGB(255, 227, 227, 227);
-  Color selectedBackground = const Color.fromARGB(255, 0, 120, 215);
+  static const Color defaultForegroundColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color defaultBackgroundColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color defaultSelectedBackgroundColor = Color.fromARGB(255, 0, 0, 255);
+  static const Color defaultMonitorBackgroundColor = Color.fromARGB(255, 200, 200, 200);
+  static const String defaultFont = 'Courier';
+  final Color foregroundColor;
+  final Color backgroundColor;
+  final Color selectedBackgroundColor;
+  final Color monitorBackgroundColor;
+  final Map<EditorComponent, Color> colors;
+  final String font;
 
-  Color dataType = const Color.fromARGB(0xFF, 255, 0, 255);
-  Color number = defaultForeground;
-  Color invalidKeyword = const Color.fromARGB(0xFF, 255, 0, 0);
-  Color keyword = const Color.fromARGB(0xFF, 0, 0, 255);
-  Color name = defaultForeground;
-  Color remark = const Color.fromARGB(0xFF, 0, 128, 0);
-  Color string = const Color.fromARGB(0xFF, 255, 0, 255);
-  Color text = defaultForeground;
-  Color textSelection = defaultForeground;
-  Color operator = defaultForeground;
-  Color lineNumber = defaultForeground;
-  Color printPageBoundaries = const Color.fromARGB(0xFF, 0, 128, 0);
-  Color lineModificationChange = const Color.fromARGB(0xFF, 255, 255, 0);
-  Color lineModificationSave = const Color.fromARGB(0xFF, 0, 128, 0);
-  Color bracesHighlight = const Color.fromARGB(0xFF, 255, 255, 0);
-  Color hypertextUrlHighlight = const Color.fromARGB(0xFF, 0, 0, 255);
-  Color codeSnippets = const Color.fromARGB(0xFF, 180, 228, 180);
-  Color columnIndentation = const Color.fromARGB(0xFF, 211, 211, 211);
-  Color includeFiles = const Color.fromARGB(0xFF, 128, 0, 0);
+  const EditorTheme({
+    this.foregroundColor = defaultForegroundColor,
+    this.backgroundColor = defaultBackgroundColor,
+    this.monitorBackgroundColor = defaultMonitorBackgroundColor,
+    this.selectedBackgroundColor = defaultSelectedBackgroundColor,
+    this.colors = const <EditorComponent, Color>{},
+    this.font = defaultFont,
+  });
 
-  String? fontName;
-
-  TextSpan createTextSpan(Component component, String text,
-      {
-        bool isSelected = false,
-        bool isMonitorMode = false
-      }) {
-    Color foreground = defaultForeground;
-    Color background = defaultBackground;
-
-    switch (component) {
-      case Component.dataType: foreground = dataType; break;
-      case Component.number: foreground = number; break;
-      case Component.invalidKeyword: foreground = invalidKeyword; break;
-      case Component.keyword: foreground = keyword; break;
-      case Component.name: foreground = name; break;
-      case Component.remark: foreground = remark; break;
-      case Component.string: foreground = string; break;
-      case Component.text: foreground = this.text; break;
-      case Component.operator: foreground = operator; break;
-      case Component.lineNumber: foreground = lineNumber; break;
-      case Component.printPageBoundaries: foreground = printPageBoundaries; break;
-      case Component.lineModificationChange: foreground = lineModificationChange; break;
-      case Component.lineModificationSave: foreground = lineModificationSave; break;
-      case Component.hypertextUrlHighlight: foreground = hypertextUrlHighlight; break;
-      case Component.codeSnippets: foreground = codeSnippets; break;
-      case Component.columnIndentation: foreground = columnIndentation; break;
-      case Component.includeFiles: foreground = includeFiles; break;
-      default: foreground = defaultForeground; break;
-    }
-
-    if (isSelected) {
-      background = selectedBackground;
-
-      // Best guess at Automation Studios automatic foreground selection
-      var c = background.red + background.green + background.blue;
-
-      if (c < ((256 / 2) * 3)) {
-        foreground = const Color.fromARGB(255, 255, 255, 255);
-      } else {
-        foreground = const Color.fromARGB(255, 0, 0, 0);
-      }
-    } else if (component == Component.bracesHighlight) {
-      background = bracesHighlight;
-      foreground = defaultForeground;
-    } else if (isMonitorMode) {
-      background = monitorBackground;
-    }
-
-    return TextSpan(
-      text: text,
-      style: TextStyle(color: foreground, backgroundColor: background),
-    );
+  Color getColor(EditorComponent component) {
+    return colors[component] ?? foregroundColor;
   }
 
+  EditorTheme copyWith({
+    Color? foregroundColor,
+    Color? backgroundColor,
+    Color? monitorBackgroundColor,
+    Color? selectedBackgroundColor,
+    Map<EditorComponent, Color>? colors,
+    String? font,
+  }) {
+    var combinedColors = Map.of(this.colors);
+    if (colors != null) {
+      for (var c in colors.keys) {
+        combinedColors[c] = colors[c] ?? this.foregroundColor;
+      }
+    }
+    return EditorTheme(
+      foregroundColor: foregroundColor ?? this.foregroundColor,
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      monitorBackgroundColor: monitorBackgroundColor ?? this.monitorBackgroundColor,
+      selectedBackgroundColor: selectedBackgroundColor ?? this.selectedBackgroundColor,
+      colors: combinedColors,
+      font: font ?? this.font,
+    );
+  }
+}
+
+// Potential pre-defined theme list
+// 'Defaults',
+// 'Afterglow',
+// 'Dracula',
+// 'Gruvbox Dark',
+// 'Gruvbox Light',
+// 'Monokai',
+// 'Nord',
+// 'Solarized Dark',
+// 'Solarized Light',
+// 'Spacegray',
+// 'Tokyo Night',
+
+class PredefinedTheme {
+  static Map<String, EditorTheme> themes = {
+    'Dracula': const EditorTheme(
+      foregroundColor: Color(0xFFF8F8F2),
+      backgroundColor: Color(0xFF282A36),
+      selectedBackgroundColor: Color(0xFF44475A),
+      colors: {
+        EditorComponent.remark: Color(0xFF6272A4),
+        EditorComponent.keyword: Color(0xFFFF79C6),
+        EditorComponent.string: Color(0xFFF1FA8C),
+        EditorComponent.dataType: Color(0xFF8BE9FD),
+        EditorComponent.name: Color(0xFFF8F8F2),
+      })
+  };
+}
+
+class EditorThemeUtils {
   static Color toColor(XmlElement xmlElement) {
     var red = int.parse(xmlElement.getAttribute('Red') ?? '0');
     var green = int.parse(xmlElement.getAttribute('Green') ?? '0');
@@ -113,19 +115,24 @@ class EditorTheme {
   }
 
   static EditorTheme fromEditorSet(File editorSet) {
-    var theme = EditorTheme();
+    Color foregroundColor = EditorTheme.defaultForegroundColor;
+    Color backgroundColor = EditorTheme.defaultBackgroundColor;
+    Color selectedBackgroundColor = EditorTheme.defaultSelectedBackgroundColor;
+    Color monitorBackgroundColor = EditorTheme.defaultMonitorBackgroundColor;
+    Map<EditorComponent, Color> colors = {};
+    String font = EditorTheme.defaultFont;
 
     final document = XmlDocument.parse(editorSet.readAsStringSync());
     final categoryNodes = document.findAllElements('Category');
     final textEditorNode = categoryNodes.firstWhere((element) =>
-      (element.getAttribute('Name') == 'TextEditor'));
+    (element.getAttribute('Name') == 'TextEditor'));
 
     for (var child in textEditorNode.children) {
       if (child is XmlElement) {
         if (child.name.local == 'Font') {
-          theme.fontName = child.getAttribute('Name');
+          font = child.getAttribute('Name') ?? 'Courier';
         } else if (child.name.local == 'BackColor') {
-          theme.defaultBackground = toColor(child);
+          backgroundColor = toColor(child);
         } else if (child.name.local == 'Items') {
           for (var item in child.children) {
             if (item is XmlElement && item.name.local == 'Item') {
@@ -133,25 +140,25 @@ class EditorTheme {
               var foreColor = item.getElement('ForeColor');
               if (foreColor != null) {
                 switch (elementType) {
-                  case 'DataType': theme.dataType = toColor(foreColor); break;
-                  case 'Number': theme.number = toColor(foreColor); break;
-                  case 'InvalidKeyword': theme.invalidKeyword = toColor(foreColor); break;
-                  case 'Keyword': theme.keyword = toColor(foreColor); break;
-                  case 'Name': theme.name = toColor(foreColor); break;
-                  case 'Remark': theme.remark = toColor(foreColor); break;
-                  case 'String': theme.string = toColor(foreColor); break;
-                  case 'Text': theme.text = toColor(foreColor); break;
-                  case 'TextSelection': theme.textSelection = toColor(foreColor); break;
-                  case 'Operator': theme.operator = toColor(foreColor); break;
-                  case 'Linenumber': theme.lineNumber = toColor(foreColor); break;
-                  case 'PrintPageBoundaries': theme.printPageBoundaries = toColor(foreColor); break;
-                  case 'LineModificatorChange': theme.lineModificationChange = toColor(foreColor); break;
-                  case 'LineModificatorSave': theme.lineModificationSave = toColor(foreColor); break;
-                  case 'BracesHighlight': theme.bracesHighlight = toColor(foreColor); break;
-                  case 'HypertextUrlHighlight': theme.hypertextUrlHighlight = toColor(foreColor); break;
-                  case 'CodeSnippets': theme.codeSnippets = toColor(foreColor); break;
-                  case 'ColumnIndentation': theme.columnIndentation = toColor(foreColor); break;
-                  case 'IncludeFiles': theme.includeFiles = toColor(foreColor); break;
+                  case 'DataType': colors[EditorComponent.dataType] = toColor(foreColor); break;
+                  case 'Number': colors[EditorComponent.number] = toColor(foreColor); break;
+                  case 'InvalidKeyword': colors[EditorComponent.invalidKeyword] = toColor(foreColor); break;
+                  case 'Keyword': colors[EditorComponent.keyword] = toColor(foreColor); break;
+                  case 'Name': colors[EditorComponent.name] = toColor(foreColor); break;
+                  case 'Remark': colors[EditorComponent.remark] = toColor(foreColor); break;
+                  case 'String': colors[EditorComponent.string] = toColor(foreColor); break;
+                  case 'Text': colors[EditorComponent.text] = toColor(foreColor); break;
+                  case 'TextSelection': selectedBackgroundColor = toColor(foreColor); break;
+                  case 'Operator': colors[EditorComponent.operator] = toColor(foreColor); break;
+                  case 'Linenumber': colors[EditorComponent.lineNumber] = toColor(foreColor); break;
+                  case 'PrintPageBoundaries': colors[EditorComponent.printPageBoundaries] = toColor(foreColor); break;
+                  case 'LineModificatorChange': colors[EditorComponent.lineModificationChange] = toColor(foreColor); break;
+                  case 'LineModificatorSave': colors[EditorComponent.lineModificationSave] = toColor(foreColor); break;
+                  case 'BracesHighlight': colors[EditorComponent.bracesHighlight] = toColor(foreColor); break;
+                  case 'HypertextUrlHighlight': colors[EditorComponent.hypertextUrlHighlight] = toColor(foreColor); break;
+                  case 'CodeSnippets': colors[EditorComponent.codeSnippets] = toColor(foreColor); break;
+                  case 'ColumnIndentation': colors[EditorComponent.columnIndentation] = toColor(foreColor); break;
+                  case 'IncludeFiles': colors[EditorComponent.includeFiles] = toColor(foreColor); break;
                   default: break;
                 }
               }
@@ -160,7 +167,14 @@ class EditorTheme {
         }
       }
     }
-    
-    return theme;
+
+    return EditorTheme(
+      foregroundColor: foregroundColor,
+      backgroundColor: backgroundColor,
+      selectedBackgroundColor: selectedBackgroundColor,
+      monitorBackgroundColor: monitorBackgroundColor,
+      colors: colors,
+      font: font
+    );
   }
 }
